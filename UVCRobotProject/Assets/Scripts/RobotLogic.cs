13250fea753c2e111232704870bd4dcc7637d7b2
liubo-home-pc <lreebom@gmail.com>
2020-05-05 23:04:43 +0800

    项目初始化

---
+++ UVCRobotProject/Assets/Scripts/RobotLogic.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotLogic : MonoBehaviour
+{
+    public GameObject[] subModuleGo;
+
+    public int uvcIndex;
+    public int songYaoIndex;
+    public int songCanIndex;
+    public int xiaoDuIndex;
+
+    void Start()
+    {
+
+    }
+}
