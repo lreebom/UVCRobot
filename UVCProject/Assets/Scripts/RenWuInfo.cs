27ffc54b1ee5c6034ec5398b80ab9015d52d54dc
liubo-work-pc <lreebom@gmail.com>
2020-05-08 18:02:32 +0800

    重写逻辑

---
+++ UVCProject/Assets/Scripts/RenWuInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenWuInfo : MonoBehaviour
+{
+    public Transform targetParent;
+
+    public Transform startEndPoint;
+
+    public Transform startEndPointOut;
+
+    public Transform shangDuanParent;
+
+    public Transform shangDuan;
+}
--- UVCProject/Assets/Scripts/RobotLogic.cs
+++ UVCProject/Assets/Scripts/RobotLogic.cs
@@ -6,14 +6,18 @@ public class RobotLogic : MonoBehaviour
 {
     public UnityEngine.AI.NavMeshAgent agent;
 
-    public GameObject[] subModuleGo;
-
     public Timer timer;
 
     public Transform targetParent;
 
     public float stopDistance;
     public float stopSpeed;
+
+    public RenWuInfo originInfo;
+
+    public RenWuInfo[] renWuInfos;
+
+    private RenWuInfo lastRenWu;
 
     void Start()
     {
@@ -31,42 +35,112 @@ public class RobotLogic : MonoBehaviour
     {
         timer.timeSpeed = 0.01f;
 
-        yield return new WaitForSeconds(0.5f);
-
-        for (int i = 0; i < subModuleGo.Length; i++)
+        if (lastRenWu != null)
         {
-            subModuleGo[i].SetActive(i == _index);
+            yield return StartCoroutine(IE_GoToTarget(lastRenWu.startEndPointOut.position));
+
+            agent.SetDestination(lastRenWu.startEndPoint.position);
+
+            LeanTween.move(gameObject, lastRenWu.startEndPoint.position, 1f);
+            LeanTween.rotateLocal(gameObject, Vector3.zero, 1f);
+
+            yield return new WaitForSeconds(1.5f);
+
+            lastRenWu.shangDuan.SetParent(lastRenWu.shangDuanParent);
+
+            TRDev.SafetyClient.Lreebom.ReSetTranform(lastRenWu.shangDuan);
+
+            yield return new WaitForSeconds(0.5f);
+
+            lastRenWu = null;
         }
 
+        lastRenWu = renWuInfos[_index];
+        Debug.Log(110);
+        yield return StartCoroutine(IE_GoToTarget(lastRenWu.startEndPointOut.position));
+        Debug.Log(111);
+        agent.SetDestination(lastRenWu.startEndPoint.position);
+        Debug.Log(112);
+        LeanTween.move(gameObject, lastRenWu.startEndPoint.position, 1f);
+        LeanTween.rotateLocal(gameObject, Vector3.zero, 1f);
+        yield return new WaitForSeconds(1.5f);
+        Debug.Log(113);
+
+        lastRenWu.shangDuan.SetParent(transform);
+
+        TRDev.SafetyClient.Lreebom.ReSetTranform(lastRenWu.shangDuan);
+
         yield return new WaitForSeconds(0.5f);
 
-        for (int i = 0; i < targetParent.childCount; i++)
+
+
+        for (int i = 0; i < lastRenWu.targetParent.childCount; i++)
         {
-            Vector3 targetPosition = targetParent.GetChild(i).position;
-
-            agent.SetDestination(targetPosition);
-
-            float distance = Vector3.Distance(transform.position, targetPosition);
-
-            while (true)
-            {
-                distance = Vector3.Distance(transform.position, targetPosition);
-
-                if (distance < stopDistance && agent.velocity.magnitude < stopSpeed)
-                {
-                    break;
-                }
-
-                yield return null;
-            }
+            yield return StartCoroutine(IE_GoToTarget(lastRenWu.targetParent.GetChild(i).position));
         }
 
-        agent.SetDestination(transform.parent.position);
+        yield return StartCoroutine(IE_GoToTarget(originInfo.startEndPointOut.position));
 
-        LeanTween.moveLocal(gameObject, Vector3.zero, 1f);
+        agent.SetDestination(originInfo.startEndPoint.position);
+
+        LeanTween.move(gameObject, originInfo.startEndPoint.position, 1f);
         LeanTween.rotateLocal(gameObject, Vector3.zero, 1f);
 
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(1.5f);
+
+
+        //yield return new WaitForSeconds(0.5f);
+
+        //for (int i = 0; i < subModuleGo.Length; i++)
+        //{
+        //    subModuleGo[i].SetActive(i == _index);
+        //}
+
+        //yield return new WaitForSeconds(0.5f);
+
+        //for (int i = 0; i < targetParent.childCount; i++)
+        //{
+        //    Vector3 targetPosition = targetParent.GetChild(i).position;
+
+        //    agent.SetDestination(targetPosition);
+
+        //    float distance = Vector3.Distance(transform.position, targetPosition);
+
+        //    while (true)
+        //    {
+        //        distance = Vector3.Distance(transform.position, targetPosition);
+
+        //        if (distance < stopDistance && agent.velocity.magnitude < stopSpeed)
+        //        {
+        //            break;
+        //        }
+
+        //        yield return null;
+        //    }
+        //}
+
+        //agent.SetDestination(transform.parent.position);
+
+        //LeanTween.moveLocal(gameObject, Vector3.zero, 1f);
+        //LeanTween.rotateLocal(gameObject, Vector3.zero, 1f);
+
+        //yield return new WaitForSeconds(0.5f);
         timer.timeSpeed = 1f;
     }
+
+    IEnumerator IE_GoToTarget(Vector3 _position)
+    {
+        agent.SetDestination(_position);
+        while (true)
+        {
+            float distance = Vector3.Distance(agent.transform.position, _position);
+            //Debug.Log(distance + "  " + agent.velocity.magnitude);
+            if (distance < stopDistance && agent.velocity.magnitude < stopSpeed)
+            {
+                break;
+            }
+            yield return null;
+        }
+    }
+
 }
